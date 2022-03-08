1217c113f9d21f195e04ce3a0333921d78323672
mateocristino <mateocristino@gmail.com>
2022-03-08 16:04:17 -0300

    Commit 2

--- main.js
+++ main.js
@@ -10,5 +10,5 @@ searchInput.addEventListener("keydown", function(event) {
 function search() {
     const input = searchInput.value;
 
-    window.location.href = "https://www.google.com/search?q=" + input + "&rlz=1C1GCEA_enUY910UY910&sxsrf=APq-WBu89sp58ZH3lFTaOZzcykSJdLxIpw%3A1646764221432&ei=vaAnYqz6GfPU1sQP756I4AQ&oq=" + input + "&gs_lcp=Cgdnd3Mtd2l6EAMYADIECCMQJzIGCCMQJxATMgQIABBDMgQIABBDMgQIABBDMgoIABCABBCHAhAUMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDoICC4QsQMQgwE6CAgAEIAEELEDOgsIABCABBCxAxCDAToHCAAQsQMQQzoECC4QQ0oECEEYAEoECEYYAFAAWKASYL0daABwAXgAgAGSAYgB6QaSAQMyLjaYAQCgAQHAAQE&sclient=gws-wiz"
+    window.location.href = "https://www.google.com/search?q=" + input + "&rlz=1C1GCEA_enUY910UY910&sxsrf=APq-WBu89sp58ZH3lFTaOZzcykSJdLxIpw%3A1646764221432&ei=vaAnYqz6GfPU1sQP756I4AQ&oq=" + input + "&gs_lcp=Cgdnd3Mtd2l6EAMYADIECCMQJzIGCCMQJxATMgQIABBDMgQIABBDMgQIABBDMgoIABCABBCHAhAUMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDoICC4QsQMQgwE6CAgAEIAEELEDOgsIABCABBCxAxCDAToHCAAQsQMQQzoECC4QQ0oECEEYAEoECEYYAFAAWKASYL0daABwAXgAgAGSAYgB6QaSAQMyLjaYAQCgAQHAAQE&sclient=gws-wiz";
 }
